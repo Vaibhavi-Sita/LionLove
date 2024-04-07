15d24792ab9278d95088978b38cbd20e712e811c
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2024-04-07 14:52:20 -0400

    init commit

--- android/app/src/main/kotlin/com/example/lion_love/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lion_love
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/routes.dart
+++
@@ -1,6 +0,0 @@
-class MyRoutes {
-  static String loginRoute = "/login";
-  static String homeRoute = "/home";
-  static String usersRoute="/user_screen";
-  static String OnboardingRoute="/onboarding_screen";
-}
